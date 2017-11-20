5c8cbaf5ad984d936777e4cfbac0f98a55f86568
Martin <Martin@10.34.26.128>
2017-11-20 21:32:56 +0100

    Ajout méthode initialise

--- Projet_Info_Puissance4/src/Initialise.java
+++ Projet_Info_Puissance4/src/Initialise.java
@@ -1,8 +1,21 @@
 
 public class Initialise {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private final static int VIDE = 0 ;
+	private final static int JAUNE = 1 ;
+	private final static int ROUGE = 2 ;
+
+	public static void Initialise(int[][] grille) {
+
+		for(int i = 0 ; i < grille.length ; i++) {
+			
+			for(int j = 0 ; j < grille[0].length ; j++) {
+
+				grille[i][j] = VIDE ;
+
+			}
+
+		}
 
 	}
 
