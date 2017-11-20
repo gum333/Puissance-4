f1de5003359a206dcd5ef2ee4e81aaece45e03f4
Martin <Martin@10.34.26.128>
2017-11-20 21:36:25 +0100

    Ajout méthode Affiche

---
+++ Projet_Info_Puissance4/src/Affiche.java
@@ -0,0 +1,46 @@
+
+public class Affiche {
+
+	public static void Affiche(int[][] grille) {
+
+		for(int[] ligne : grille) {
+
+			System.out.print(" |") ;
+
+			for(int cellule : ligne) {
+
+				if (cellule == VIDE) {
+
+					System.out.print(" ");
+
+				} else if (cellule == ROUGE) {
+
+					System.out.print("O");
+
+				} else if (cellule == JAUNE) {
+
+					System.out.print("X");
+
+				}
+
+				System.out.print("|");
+
+			}
+
+			System.out.println();
+
+		}
+
+		System.out.print("=");
+
+		for(int i = 1 ; i <= grille[0].length ; i++) {
+
+			System.out.print("=" + i);
+
+		}
+
+		System.out.println("==\n");
+
+	}
+
+}
